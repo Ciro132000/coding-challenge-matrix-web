d72f298cf9730998ca8e6be7622407651bb27d97
Ciro <c.g_ciro@hotmail.com>
2026-06-19 19:55:59 -0500

    FT002: MEJORA DE UI Y FUNCION PARA LEVANTAR SERVICIOS

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import './App.css'
 import { AuthProvider } from './core/auth/AuthProvider'
 import { QueryProvider } from './providers/QueryProvider'
@@ -5,6 +6,28 @@ import { QueryProvider } from './providers/QueryProvider'
 import { AppRouter } from './router/AppRouter'
 
 function App() {
+
+  useEffect(() => {
+    if(import.meta.env.VITE_ENV == "production") {
+      const nodeApi = import.meta.env.VITE_NODE_API__URL ?? "";
+      const goApi = import.meta.env.VITE_GO_API_URL ?? "";
+
+      if(nodeApi != "" && goApi != "") {
+        const runService = async () =>{
+          Promise.allSettled([
+            fetch(nodeApi),
+            fetch(goApi)
+          ]).then(() => {
+            console.log("Servicios ok")
+          }).catch(() => {
+            console.log("Servicio no disponible")
+          })
+        }
+
+        runService();
+      }
+    }
+  }, [])
 
   return (
     <AuthProvider>
